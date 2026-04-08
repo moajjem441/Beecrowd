b8d61c7ecf145d0b5d6703eb5211324152b56e7d
Moajjem Hossain <moajjem441@gmail.com>
2026-04-08 22:41:14 +0600

    one more code

---
+++ 1067.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    int x=1;
+   while(x<=n)
+   {
+   if(x%2!=0)
+   cout<<x<<endl;
+    x++;
+   }
+    return 0;
+}
